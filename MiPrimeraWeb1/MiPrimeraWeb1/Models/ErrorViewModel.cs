5213dbff36602d1dec98eb951fbfd0201edbfda4
OscarArias1234 <42899156+OscarArias1234@users.noreply.github.com>
2018-09-01 16:21:50 -0500

    MiPrimeraSubida

---
+++ MiPrimeraWeb1/MiPrimeraWeb1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiPrimeraWeb1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
